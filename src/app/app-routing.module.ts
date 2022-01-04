32ad971c0f2153f4fa7ebcd1b3f6e41da4a5f5a4
Petrov Ilya <dnamix1@gmail.com>
2022-01-04 13:10:16 +0300

    entry point for an app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {CreateArticleComponent} from './create-article/components/create-article/create-article.component'
+import {ArticleComponent} from './article/components/article/article.component'
+import {FeedComponent} from './feed/feed.component'
+import {RegisterComponent} from './auth/components/register/register.component'
+import {NgModule} from '@angular/core'
+import {RouterModule, Routes} from '@angular/router'
+import {SubscriptionFeedComponent} from './subscription-feed/subscription-feed.component'
+import {TagFeedComponent} from './tagFeed/tagFeed.component'
+import {EditArticleComponent} from './edit-article/components/edit-article/edit-article.component'
+import {SettingsComponent} from './settings/components/settings/settings.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', component: RegisterComponent},
+  {path: 'articles/feed', component: SubscriptionFeedComponent},
+  {path: 'articles/new', component: CreateArticleComponent},
+  {path: 'articles/edit/:slug', component: EditArticleComponent},
+  {path: 'articles/:slug', component: ArticleComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: 'tags/:tag', component: TagFeedComponent},
+  {path: '', component: FeedComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,63 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {FavoritesModule} from './common/favorites/favorites.module'
+import {SettingsModule} from './settings/settings.module'
+import {EditArticleModule} from './edit-article/edit-article.module'
+import {CreateArticleModule} from './create-article/create-article.module'
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {AppRoutingModule} from './app-routing.module'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {TagFeedModule} from './tagFeed/tagfeed.module'
+import {SubscriptionFeedModule} from './subscription-feed/subscription-feed.module'
+import {AuthInterceptor} from './shared/services/authInterceptor.service'
+import {PersistanceService} from './shared/services/persistance.service'
+import {NavBarModule} from './common/navbar/NavBar.module'
+import {EffectsModule} from '@ngrx/effects'
+import {environment} from './../environments/environment'
+import {FeedModule} from './feed/feed.module'
+import {SingleArticleModule} from './article/single-article.module'
+
+import {AuthModule} from './auth/auth.module'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
+
+import {AppComponent} from 'src/app/app.component'
+import {StoreModule} from '@ngrx/store'
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
+import {routerReducer, StoreRouterConnectingModule} from '@ngrx/router-store'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NavBarModule,
+    AuthModule,
+    FeedModule,
+    TagFeedModule,
+    SingleArticleModule,
+    SubscriptionFeedModule,
+    CreateArticleModule,
+    EditArticleModule,
+    SettingsModule,
+    FavoritesModule,
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot({router: routerReducer}),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    PersistanceService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
